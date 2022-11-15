dd44880ea1b8799d80767decac71500f218a06db
berkeehalil <bayraktarbh@gmail.com>
2022-11-15 14:38:15 +0300

    create delete item function

--- assets/script.js
+++ assets/script.js
@@ -12,26 +12,29 @@ const taskList = document.querySelector("#task-list");
 eventListeners();
 
 function eventListeners() {
+
+    // submit event
     form.addEventListener("submit", addNewItem);
+
+    // delete event
+    taskList.addEventListener("click", deleteItem);
 }
 
+// add item
 function addNewItem(e) {
-    if(input.value == ''){
+    if (input.value == '') {
         alert("add new item");
-        // console.log("submit");
     }
 
     // create li
-
     const li = document.createElement("li");
     li.className = "list-group-item list-group-item-secondary";
     li.appendChild(document.createTextNode(input.value));
 
     // create a
-
     const a = document.createElement("a");
     a.classList = "delete-item float-right";
-    a.setAttribute("href","#");
+    a.setAttribute("href", "#");
     a.innerHTML = '<i class="fas fa-times"></i>';
 
     li.appendChild(a);
@@ -43,3 +46,10 @@ function addNewItem(e) {
     e.preventDefault();
 }
 
+// delete item
+
+function deleteItem(e) {
+    if(e.target.className == "fas fa-times"){
+        e.target.parentElement.parentElement.remove();
+    }
+}
