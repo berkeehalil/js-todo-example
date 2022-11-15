5bfe86e77b11211b2eee22aa778e20abca08a6d7
berkeehalil <bayraktarbh@gmail.com>
2022-11-15 14:14:03 +0300

    selection has been made

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+//TODO add elements
+
+//element selection
+
+const form = document.querySelector("form");
+const input = document.querySelector("#txtTaskName");
+const btnAddNewTask = document.querySelector("#btnAddNewTask");
+const btnDeleteAll = document.querySelector("#btnDeleteAll");
+const taskList = document.querySelector("#task-list");
+
